b4e34de4bff84d28fd49726d53561b78c4a0966e
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-01 01:30:59 +0300

    Update desktop.py

--- desktop.py
+++ desktop.py
@@ -24,7 +24,7 @@ def update_files():
     		if not fn.endswith(".pyc"): nfiles.append(path.join(root, fn).removeprefix(".\\"))
 
     for n in nfiles:
-        print(n, x.index(n))
+        print(n, xfiles.index(n))
 
     return nfiles, files
 
