42aec28635033d337e76bc70b391f89007d8d199
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-01 02:11:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Module Imports
-import base64, flask, dash_mantine_components as dmc
+import base64, flask, dash_mantine_components as dmc, sys
 from dash import Dash, page_container, Output, Input, State, html
 from dash_auth.auth import Auth
 from datetime import datetime
@@ -67,8 +67,169 @@ class MHALAuth(Auth):
 # App
 app = Dash(__name__, use_pages=True, title="MHAL Panel", update_title="MHAL Panel", pages_folder=path.join(path.dirname(__name__), "pages"), meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0,"}])
 server = app.server
-auth = MHALAuth(app)
+if sys.platform.startswith("win"): pass
+else: auth = MHALAuth(app)
 app._favicon = "favicon.png"
+
+@server.errorhandler(500)
+def internal_server_error(e):
+    return """
+    <div class="container">
+    <a class="button" href="#" onClick='window.location.reload();' style="--color:#1971C2;">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Oturumu Başlat
+    </a>
+    </div>
+
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap');
+
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    }
+
+    .container {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 120px;
+    background: #1A1B1E;
+    }
+
+    .button {
+    position: relative;
+    padding: 16px 30px;
+    font-size: 1.5rem;
+    color: var(--color);
+    border: 2px solid #25262B;
+    border-radius: 4px;
+    text-shadow: 0 0 15px var(--color);
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+    transition: 0.5s;
+    z-index: 1;
+    background-color: #25262B;
+    }
+
+    .button:hover {
+    color: #fff;
+    border: 2px solid rgba(0, 0, 0, 0);
+    box-shadow: 0 0 0px var(--color);
+    }
+
+    .button::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--color);
+    z-index: -1;
+    transform: scale(0);
+    transition: 0.5s;
+    }
+
+    .button:hover::before {
+    transform: scale(1);
+    transition-delay: 0.5s;
+    box-shadow: 0 0 10px var(--color),
+        0 0 30px var(--color),
+        0 0 60px var(--color);
+    }
+
+    .button span {
+    position: absolute;
+    background: var(--color);
+    pointer-events: none;
+    border-radius: 2px;
+    box-shadow: 0 0 10px var(--color),
+        0 0 20px var(--color),
+        0 0 30px var(--color),
+        0 0 50px var(--color),
+        0 0 100px var(--color);
+    transition: 0.5s ease-in-out;
+    transition-delay: 0.25s;
+    }
+
+    .button:hover span {
+    opacity: 0;
+    transition-delay: 0s;
+    }
+
+    .button span:nth-child(1),
+    .button span:nth-child(3) {
+    width: 40px;
+    height: 4px;
+    }
+
+    .button:hover span:nth-child(1),
+    .button:hover span:nth-child(3) {
+    transform: translateX(0);
+    }
+
+    .button span:nth-child(2),
+    .button span:nth-child(4) {
+    width: 4px;
+    height: 40px;
+    }
+
+    .button:hover span:nth-child(1),
+    .button:hover span:nth-child(3) {
+    transform: translateY(0);
+    }
+
+    .button span:nth-child(1) {
+    top: calc(50% - 2px);
+    left: -50px;
+    transform-origin: left;
+    }
+
+    .button:hover span:nth-child(1) {
+    left: 50%;
+    }
+
+    .button span:nth-child(3) {
+    top: calc(50% - 2px);
+    right: -50px;
+    transform-origin: right;
+    }
+
+    .button:hover span:nth-child(3) {
+    right: 50%;
+    }
+
+    .button span:nth-child(2) {
+    left: calc(50% - 2px);
+    top: -50px;
+    transform-origin: top;
+    }
+
+    .button:hover span:nth-child(2) {
+    top: 50%;
+    }
+
+    .button span:nth-child(4) {
+    left: calc(50% - 2px);
+    bottom: -50px;
+    transform-origin: bottom;
+    }
+
+    .button:hover span:nth-child(4 ) {
+    bottom: 50%;
+    }
+    </style>
+    """
 
 app.layout = dmc.MantineProvider(
     children=[
