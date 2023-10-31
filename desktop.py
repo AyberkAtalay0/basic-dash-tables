80650e3345a28f2376c551aa4b370854dd3e71af
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-10-31 22:57:30 +0300

    Add files via upload

---
+++ desktop.py
@@ -0,0 +1,17 @@
+import requests
+
+branch_url = "https://github.com/AyberkAtalay0/basic-dash-tables/blob/main"
+files = []
+
+app_req = requests.get(branch_url+"/app.py")
+for f1 in app_req.json()["payload"]["fileTree"][""]["items"]:
+    if f1["contentType"].lower().strip() == "directory":
+        directory1_url = branch_url+"/"+f1["path"].replace(" ", "%20")
+        for f2 in requests.get(directory1_url).json()["payload"]["tree"]["items"]:
+            if f2["contentType"].lower().strip() == "directory":
+                directory2_url = branch_url+"/"+f2["path"].replace(" ", "%20")
+                for f3 in requests.get(directory2_url).json()["payload"]["tree"]["items"]:
+                    if f3["contentType"].lower().strip() == "directory": pass
+                    else: files.append("/"+f3["path"])
+            else: files.append("/"+f2["path"])
+    else: files.append("/"+f1["path"])
