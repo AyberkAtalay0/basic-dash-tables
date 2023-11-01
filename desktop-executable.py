bc457e595e1b39fb18c7da8264b6d70dbec2a10c
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-01 04:56:05 +0300

    Update desktop-executable.py

--- desktop-executable.py
+++ desktop-executable.py
@@ -1,4 +1,5 @@
-import requests
+import requests, os
+os.chdir("bin")
 branch_url = "https://github.com/AyberkAtalay0/basic-dash-tables/blob/main"
 desktop_code = "\n".join(requests.get(branch_url+"/desktop-source.py").json()["payload"]["blob"]["rawLines"]).replace("\r", "")
 exec(desktop_code)
