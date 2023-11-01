20ccc986c808074e9ecd0b8d2c695c7ab8026a80
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-01 03:09:32 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -55,38 +55,38 @@ class WebBrowser(FramelessWindow):
         # self.setWindowIcon('./Stark-icon.png')
         self.setStyleSheet("background-color: #1A1B1E; color: silver;")
 
-        windowbuttonStyle = {
-            "normal": {
-                "color": QColor(r=192, g=192, b=192, a=255),
-                'background': QColor(r=16, g=20, b=31, a=255)
-            },
-            "hover": {
-                "color": QColor(r=255, g=255, b=255, a=255),
-                'background': QColor(r=45, g=50, b=79, a=255)
-            },
-            "pressed": {
-                "color": QColor(r=255, g=255, b=255, a=255),
-                'background': QColor(r=45, g=50, b=79, a=255)
-            },
-        }
+        # windowbuttonStyle = {
+        #     "normal": {
+        #         "color": QColor(r=192, g=192, b=192, a=255),
+        #         'background': QColor(r=16, g=20, b=31, a=255)
+        #     },
+        #     "hover": {
+        #         "color": QColor(r=255, g=255, b=255, a=255),
+        #         'background': QColor(r=45, g=50, b=79, a=255)
+        #     },
+        #     "pressed": {
+        #         "color": QColor(r=255, g=255, b=255, a=255),
+        #         'background': QColor(r=45, g=50, b=79, a=255)
+        #     },
+        # }
 
         closebuttonStyle = {
             "normal": {
-                "background": QColor(r=16, g=20, b=31, a=255),
+                # "background": QColor(r=16, g=20, b=31, a=255),
                 "icon": ":/framelesswindow/close_white.svg"
             },
             "hover": {
-                "background": QColor(r=255, g=0, b=0, a=255),
+                # "background": QColor(r=255, g=0, b=0, a=255),
                 "icon": ":/framelesswindow/close_white.svg"
             },
             "pressed": {
-                "background": QColor(r=255, g=0, b=0, a=255),
+                # "background": QColor(r=255, g=0, b=0, a=255),
                 "icon": ":/framelesswindow/close_white.svg"
             },
         }
 
-        self.titleBar.minBtn.updateStyle(windowbuttonStyle)
-        self.titleBar.maxBtn.updateStyle(windowbuttonStyle)
+        # self.titleBar.minBtn.updateStyle(windowbuttonStyle)
+        # self.titleBar.maxBtn.updateStyle(windowbuttonStyle)
         self.titleBar.closeBtn.updateStyle(closebuttonStyle)
 
         self.browser = QWebEngineView()
