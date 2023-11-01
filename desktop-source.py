1ebe1fb0ceab4f1f43ae3ddc0484954d7feaaf10
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-01 21:05:41 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -56,6 +56,7 @@ from PyQt5.QtGui import QFont, QColor, QIcon
 from PyQt5.QtWebEngineWidgets import *
 from qframelesswindow import FramelessWindow
 
+from dash import page_container, html
 import base64
 from datetime import datetime
 from os import listdir, path, getcwd
