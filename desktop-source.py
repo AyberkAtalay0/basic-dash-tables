ef9877eee16d37330b722422366fe1a8908d102a
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-02 23:33:46 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -43,8 +43,7 @@ def update_files():
             try:
                 print(xf.removeprefix("\\"), "checking up...")
                 xsize = int(float(requests.head(branch_url.replace("https://github.com", "https://raw.githubusercontent.com").replace("/blob/", "/")+xf.replace("\\","/")).headers["Content-Length"]))
-                print(branch_url.replace("https://github.com", "https://raw.githubusercontent.com")+xf.replace("\\","/"))
-                with open(xf.removeprefix("\\"), "rb") as frb: nsize = len(frb.read())
+                with open(xf.removeprefix("\\"), "r") as frb: nsize = len(frb.read())
                 print(nsize, xsize)
                 if nsize != xsize: download_file(xf)
             except Exception as enx: 
