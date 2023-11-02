03e58860d0054b22dc74810e55a10507f41cdc4b
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-02 23:09:02 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -42,7 +42,7 @@ def update_files():
         if xf in nfiles:
             try:
                 print(xf.removeprefix("\\"), "checking up...")
-                xsize = requests.head(branch_url+xf.replace("\\","/")+"?raw=true") # int(float(requests.head(branch_url+xf.replace("\\","/")+"?raw=true").get("content-length", -1))) # len(requests.get(branch_url+xf.replace("\\","/")+"?raw=true").content)
+                xsize = requests.head(branch_url+xf.replace("\\","/")+"?raw=true").content # int(float(requests.head(branch_url+xf.replace("\\","/")+"?raw=true").get("content-length", -1))) # len(requests.get(branch_url+xf.replace("\\","/")+"?raw=true").content)
                 with open(xf.removeprefix("\\"), "rb") as frb: nsize = len(frb.read())
                 print(nsize, xsize)
                 if nsize != xsize: download_file(xf)
