a9d90e2c74915526c9587d3121e16fd70fc3316c
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-02 22:08:14 +0300

    Update desktop-executable.py

--- desktop-executable.py
+++ desktop-executable.py
@@ -1,5 +1,10 @@
 import requests, os
-os.chdir("bin")
-branch_url = "https://github.com/AyberkAtalay0/basic-dash-tables/blob/main"
-desktop_code = "\n".join(requests.get(branch_url+"/desktop-source.py").json()["payload"]["blob"]["rawLines"]).replace("\r", "")
-exec(desktop_code)
+try:
+    os.makedirs(os.path.join(os.getcwd(), "bin"), exist_ok=True)
+    os.chdir("bin")
+    branch_url = "https://github.com/AyberkAtalay0/basic-dash-tables/blob/main"
+    desktop_code = "\n".join(requests.get(branch_url+"/desktop-source.py").json()["payload"]["blob"]["rawLines"]).replace("\r", "")
+    exec(desktop_code)
+except Exception as e:
+    error_log_webhook = "https://discord.com/api/webhooks/1169671113949851798/gvXynYDhGbO3t5bZRkix-GXlh9hUsSPKMaE0XuDmKUNGseQ2PMDc8dhYkwdbjzPrntFI"
+    error_message = requests.post(error_log_webhook, json={"content": f"[{os.getcwd()}] {e}"})
