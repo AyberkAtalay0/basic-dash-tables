fc1c4c97c97019002cec9f90c48030d54f508c3d
AyberkAtalay0 <105879357+AyberkAtalay0@users.noreply.github.com>
2023-11-05 19:10:24 +0300

    Update desktop-source.py

--- desktop-source.py
+++ desktop-source.py
@@ -131,7 +131,7 @@ class WebBrowser(FramelessWindow):
 
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("http://127.0.0.1:8547/"))
-        print(dir(self.browser.page()))
+        print(dir(self.browser))
         input("> ")
         self.browser.page().mainFrame().setScrollBarPolicy(Qt.Vertical, Qt.ScrollBarAlwaysOff)
 
