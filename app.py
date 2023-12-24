5ceedb4514553d2cce83f59a74dc84a4b0eb9dd7
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2023-12-24 17:56:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def ogrenci_filtrele(n_clicks, ograd, ogrno, ogrsn):
                     if (ud(str(ograd).replace("None","").replace(" ","").upper()) in ud(str(isim).replace(" ","").upper()) or ud(str(ograd).replace("None","").replace(" ","").upper()) == "") \
                         and (ud(str(ogrno).replace("None","").replace(" ","").upper()) in ud(str(numara).replace(" ","").upper()) or ud(str(ogrno).replace("None","").replace(" ","").upper()) == "") \
                             and (ud(str(ogrsn).replace("None","").replace(" ","").upper()) in ud(str(sinif).replace(" ","").upper()) or ud(str(ogrsn).replace("None","").replace(" ","").upper()) == ""):
-                        if str(isim).split()[0].strip() == isim.strip(): isim = str(isim) + " X"
+                        if str(isim).split()[0].strip() == str(isim).strip(): isim = str(isim) + " X"
                         elem = html.Tr([html.Td(str(numara)), html.Td(isim.upper()), html.Td(sinif), html.A(children=dmc.Button("Profil"), href=f"/ogrenci/{numara}_{ud(isim.lower().replace(' ',''))}")])
                         if not (numara, isim, sinif) in readies:
                             out.append(elem)
